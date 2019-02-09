c44fb77c77b314e465cc1d363a828ffbbba1d534
dhanush199 <dhanushsh1995@gmail.com>
2019-02-09 13:01:10 +0530

    user validation done

--- src/main/java/com/bridgelabz/spring/controller/UserController.java
+++ src/main/java/com/bridgelabz/spring/controller/UserController.java
@@ -1,17 +1,25 @@
 package com.bridgelabz.spring.controller;
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.annotation.Validated;
 
 import com.bridgelabz.spring.model.User;
 import com.bridgelabz.spring.service.UserServiceInf;
@@ -21,9 +29,32 @@ public class UserController {
 
 	@Autowired
 	private UserServiceInf userService;
+	////////////////////////
+	////////////////////////
+	//////////////////////////
+User users = new User();
+	
+	@Autowired
+	@Qualifier("userValidator")
+	private org.springframework.validation.Validator validator;
+	
+	@InitBinder
+	private void initBinder(WebDataBinder binder) {
+		binder.setValidator( validator);
+	}
+	
+	@GetMapping("index")
+	public ModelAndView register(User user) {
+		return new ModelAndView("register");
+	}
 
+	
+	//////////////////////////
+	/////////////////////////
+	//////////////////////////
+	///////////////////////
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public ResponseEntity<?> registerUser(@RequestBody User user, HttpServletRequest request,HttpServletResponse resp) {
+	public ResponseEntity<?> registerUser(@Validated @RequestBody User user, HttpServletRequest request,HttpServletResponse resp) {
 		if (userService.register(user, request,resp))
 			return new ResponseEntity<String>("Successfully Updated",HttpStatus.OK);
 		else
